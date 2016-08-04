12d40c55bb433ebc6c0dc753e76eafcf232283a9
mengzhu <mengzhu@ngs.tech>
2016-08-04 15:14:20 +0800

    merge

--- src/app/component/pages.component.ts
+++ src/app/component/pages.component.ts
@@ -1,40 +1,8 @@
 import { Component,ElementRef} from '@angular/core';
   import {Router}    from '@angular/router';
 
-<<<<<<< HEAD
 @Component({
   selector: 'pages',
-
-=======
-// import { BaPageTop, BaContentTop, BaSidebar, BaBackTop } from '../theme/components';
-
-
-// import { Managers } from './manager/manager.component';
-import { MenusService } from './menus.service';
-import { MenusModel } from './menus.model';
-import { AppState } from './../app.state';
-@Component({
-  selector: 'pages',
-  encapsulation: ViewEncapsulation.None,
-  styles: [require('./pages.scss')],
-  // directives: [BaPageTop, BaSidebar, BaContentTop, BaBackTop],
-  template: require('./pages.html'),
-  providers:[MenusService],
-  // animations: [
-  //    trigger('heroState', [
-  //      state('inactive', style({
-  //        backgroundColor: '#eee',
-  //        transform: 'scale(1)'
-  //      })),
-  //      state('active',   style({
-  //        backgroundColor: '#cfd8dc',
-  //        transform: 'scale(1.1)'
-  //      })),
-  //      transition('inactive => active', animate('100ms ease-in')),
-  //      transition('active => inactive', animate('100ms ease-out'))
-  //    ])
-  //  ]
->>>>>>> b3d0c0fe39c2a1f50af85b5bef1a63a91e5e2c8a
 })
 export class Pages {
 
